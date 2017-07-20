58822c9246e4a7cfcd41d44a27cf7fe2c8cd818d
Mudith Chathuranga <mudithchathuranga@gmail.com>
2017-07-20 20:22:36 +0530

    Initial commit

---
+++ SwiftySideMenuO/SwiftySideMenuInfo.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftySideMenuInfo.swift
+//  event360-vendor-ios-app
+//
+//  Created by Mudith Chathuranga on 5/30/17.
+//  Copyright © 2017 Suren Dias. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class SwiftySideMenuInfo {
+    
+    public static let shared = SwiftySideMenuInfo()
+    
+    private init() { }
+    
+    
+    public var swiftySideMenu : SwiftySideMenuViewController!
+}
